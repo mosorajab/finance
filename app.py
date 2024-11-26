a913a9236c5e347fba2d2726b8ec842773e408e7
Mohamed Sohail Rajab <82951435+mosorajab@users.noreply.github.com>
2024-11-26 23:18:59 +0200

    Update app.py

--- app.py
+++ app.py
@@ -19,7 +19,7 @@ st.markdown("""
         font-weight: 600;
         margin-top: 10px;
         margin-bottom: 10px;
-        color: navy;
+        color: green;
     }
     /* Card styles */
     .card {
